1002895c57b6f32508ca0f074156ec212218d8f9
Julio Cesar Yepes Ruiz <julio.yepes@3ysolutions.com.co>
2020-03-26 08:31:13 -0500

    Add project files.

---
+++ self-host-webapi/DirectoriesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace self_host_webapi
+{
+    public class DirectoriesController : ApiController
+    {
+        public string[] GetDirectories()
+        {
+            return System.IO.Directory.GetDirectories(@"C:\");
+        }
+    }
+}
---
+++ self-host-webapi/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Http.SelfHost;
+
+namespace self_host_webapi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new HttpSelfHostConfiguration("http://localhost:8080");
+
+            config.Routes.MapHttpRoute(
+                "API Default", "api/{controller}/{id}",
+                new { id = RouteParameter.Optional });
+
+            var cors = new EnableCorsAttribute(origins: "https://localhost:44331", headers: "*", methods: "*");
+            config.EnableCors(cors);
+
+            using (HttpSelfHostServer server = new HttpSelfHostServer(config))
+            {
+                server.OpenAsync().Wait();
+                Console.WriteLine("Press Enter to quit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
